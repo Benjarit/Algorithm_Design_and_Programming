58cf170fcc3e79ad9404dfe3cfb94c5dcfd33d27
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2018-04-11 22:20:57 -0500

    Add files via upload

---
+++ HomeWork 3/bossFunctions.c
@@ -0,0 +1,12 @@
+#include "game.h" 
+
+bossDecision* addDecisionNodeToList(bossDecision* top, bossDecision* newTop)
+{
+}
+bossDecisionNode* addNodeToDecisionTree(bossDecisionNode* root, bossDecisionNode* newNode)
+{
+}
+
+bossDecision* getNewListInTree(bossDecisionNode* root, int currentBossHealth)
+{
+}
