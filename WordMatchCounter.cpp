c219c3fcd1b1cd686d219516c50f6c762aece5fa
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2018-10-27 23:15:55 -0500

    Update WordMatchCounter.cpp

--- WordMatchCounter.cpp
+++ WordMatchCounter.cpp
@@ -24,7 +24,6 @@ int main()
     while (myfile >> output) {
         string result;
         regex_replace (back_inserter(result), output.begin(), output.end(), first, "");
-        
         /* 
             If key already exists in the map, then do incrementation
             If key is not in the map, then initialize mapped value to 1
