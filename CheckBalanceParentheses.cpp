4618c8e860c611ce586d7ed02bbb580359872566
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2018-10-31 21:59:02 -0500

    Create CheckBalanceParentheses.cpp

---
+++ CheckBalanceParentheses.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                       
+
+*******************************************************************************/
+
+#include <iostream>
+#include <cstdlib>    /* srand, rand */
+#include <ctime>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isBalancePar(string);
+int main()
+{
+    string par = "{{}}";
+    if(isBalancePar(par)){
+        cout << "Balance!!" << endl;
+    }
+    return 0;
+}
+bool isBalancePar(string par){
+    if(par.size() == 0 || par.size() == 1){
+      return false; 
+    }else if (par.size() == 2){
+        if(par[0] == '{' && par[1] == '}'){
+            return true;
+        }
+        return false;
+    }
+    /* This will create *Short-circuit* as second condition will not be evaluated */
+    // return false && (isBalancePar(par.substr(1,par.size()-2)));
+    return (par[0] == '{' && par[par.size()-1] == '}') && (isBalancePar(par.substr(1,par.size()-2)));
+}
