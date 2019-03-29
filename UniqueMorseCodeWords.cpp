723889ffceb67934171fddd2fc9c51b89a105324
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-03-29 00:11:22 -0500

    Update UniqueMorseCodeWords.cpp

--- UniqueMorseCodeWords.cpp
+++ UniqueMorseCodeWords.cpp
@@ -4,10 +4,7 @@
   * Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cba" can be written as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
 
   * Return the number of different transformations among all words we have.
-
 */
-
-
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
